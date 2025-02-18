2d4ab7f389d60bc9ebe7e801dc7584b7bd267620
Basile Parent <casou.acc@gmail.com>
2025-02-18 12:27:18 +0100

    Change slide build to slides.html

--- vite.config.slides.ts
+++ vite.config.slides.ts
@@ -1,11 +1,28 @@
 import path from "path"
-import { defineConfig } from "vite"
+import { defineConfig, Plugin } from "vite"
+import {NormalizedOutputOptions, OutputBundle} from "rollup";
+
+const renameIndexPlugin = (newFilename: string): Plugin | undefined => {
+    if (!newFilename) return
+
+    return {
+        name: "renameIndex",
+        enforce: "post",
+        generateBundle: (_: NormalizedOutputOptions, bundle: OutputBundle) => {
+            const indexHtml = bundle["index.html"]
+            indexHtml.fileName = newFilename
+        },
+    }
+}
 
 export default defineConfig({
     root: path.resolve(__dirname, "src/slides"),
-    base: "/a11y-formation/slides/",
+    base: "/a11y-formation/",
     build: {
-        outDir: '../../dist/slides',
+        outDir: "../../dist",
         emptyOutDir: true,
     },
+    plugins: [
+        renameIndexPlugin("slides.html")
+    ]
 })
